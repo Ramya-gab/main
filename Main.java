ebed91ded7d807c7a2585f6766c6ce7ff287592c
ramya <ramyagabbur@gmail.com>
2025-04-29 10:47:30 +0530

    java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+public class Main {
+ 
+    public static void main(String[] args){
+    int a=5;
+    int b=10;
+    System.out.println("Addition :"+(a+b));
+    System.out.println("sub :"+(a-b));
+    System.out.println("mul :"+(a*b));
+    System.out.println("div :"+(a/b));
+
+}
+}
